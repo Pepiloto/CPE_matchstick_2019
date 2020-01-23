1fcaf96d3f6140a1d2e9f20cebdf2c16689207e0
Paul BEAUDET <pepiloto@localhost.localdomain>
2020-01-23 11:01:57 +0100

    [add] The -h (--help) message

--- src/error.c
+++ src/error.c
@@ -7,6 +7,21 @@
 
 #include "my.h"
 
+int bad_argument(char **av)
+{
+    if (my_strcmp(av[1], "-h") == 0 || my_strcmp(av[1], "--help") == 0) {
+        my_putstr("Little matchstick game\n\nUSAGE\n./matchstick [nb_lines] [nb"
+        "_maximum_of_matches_to_be_removed]\n");
+        return (0);
+    }
+    if (my_str_isnum(av[1]) == 0 || my_str_isnum(av[2]) == 0) {
+        my_puterrstr("./matchstick : bad arguments : one of the arguments is no"
+                    "t a number\nretry with -h or --help\n");
+        return (84);
+    }
+    return (0);
+}
+
 int error_handling(int ac, char **av)
 {
     if (ac > 3) {
@@ -19,10 +34,7 @@ int error_handling(int ac, char **av)
                     "s required\nretry with -h or --help\n");
         return (84);
     }
-    if (my_str_isnum(av[1]) == 0 || my_str_isnum(av[2]) == 0) {
-        my_puterrstr("./matchstick : bad arguments : one of the arguments is no"
-                    "t a number\nretry with -h or --help\n");
+    if (bad_argument(av) == 84)
         return (84);
-    }
     return (0);
 }
